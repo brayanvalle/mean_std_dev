82b434c7ff6306865c86ea206430296059a9a4ef
Brayan Valle <brayanvallejaramillo@gmail.com>
2018-08-06 23:05:59 -0500

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,97 +1,89 @@
-$( document ).ready(function(){
-    
+$(document).ready(function() {
+
     var _data = []
     var _data = '186,699,132,272,291,331,199,1890,788,1601'
 
-    $('#calculate').click(function(){
-        data = $('#input_numbers').val()
+    $('#calculate').click(function() {
+         data = $('#input_numbers').val()
         _data = separateData(data)
-        if(_data.length > 3){
+        if (_data.length > 3) {
             result = calculate(_data)
             console.log(result)
-            if(result == 0){
+            if (result == 0) {
                 $('.messages').html("There was an error")
                 cleanUp()
                 return
             }
-            if(result.mean != 'NaN' && result.std_deviation != 'NaN'){
+            if (result.mean != 'NaN' && result.std_deviation != 'NaN') {
                 addResult(result)
                 $('.messages').html("")
-            }else{
+            } else {
                 $('.messages').html("There was an error")
                 cleanUp()
             }
-        }else{
+        } else {
             $('.messages').html("Error: Enter at least 4 values")
             cleanUp()
         }
     });
 
-    function cleanUp(){
+    function cleanUp() {
         $('#result_count').html("")
         $('#result_mean').html("")
         $('#result_std_deviation').html("")
     }
 
-    function addResult(result){
+    function addResult(result) {
         $('#result_count').html(result.count)
         $('#result_mean').html(result.mean)
         $('#result_std_deviation').html(result.std_deviation)
     }
-    
 
-    function calculate(data){
-        
 
-        if(data == 0)
+    function calculate(data) {
+
+
+        if (data == 0)
             return "Error"
-        else{
+        else {
             mean = calculateMean(data)
             std_deviation = calculateStandardDeviation(data)
 
             return response = ({
-                    'count' : data.length,
-                    'mean' : mean,
-                    'std_deviation' : std_deviation
+                'count': data.length,
+                'mean': mean,
+                'std_deviation': std_deviation
             });
-        }           
+        }
 
     }
 
-    function calculateMean(_data){
+    function calculateMean(_data) {
         return (_data.reduce((a, b) => a + b, 0) / _data.length).toFixed(2)
     }
 
-    function calculateStandardDeviation(_data){
+    function calculateStandardDeviation(_data) {
 
         var sum = 0
         mean = calculateMean(_data)
         _data.forEach(number => {
-            sum +=Math.pow( number - mean, 2)
+            sum += Math.pow(number - mean, 2)
         });
 
         std = Math.sqrt(sum / (_data.length - 1))
         return std.toFixed(2)
-
     }
 
 
-    function separateData(data){
-        if(data == null || data == undefined || data == "empty")
-            return 0        
+    function separateData(data) {
+        if (data == null || data == undefined || data == "empty")
+            return 0
         else
-            return data.split(",").map(Number)          
+            return data.split(",").map(Number)
     }
 
-
-
-    function print(x){
+    function print(x) {
         console.log(x)
     }
-
-
-    
-
-   
 
 });
